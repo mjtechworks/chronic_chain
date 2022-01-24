965621c222f2cec71d5cce26ad37fb07b44cd493
mjtechworks <michaeljohnson00518@gmail.com>
2022-01-24 13:18:00 -0500

    bug fixed

---
+++ vue/src/common/lunie-message-types.js
@@ -0,0 +1,13 @@
+const lunieMessageTypes = {
+  SEND: `SendTx`,
+  STAKE: `StakeTx`,
+  RESTAKE: `RestakeTx`,
+  UNSTAKE: `UnstakeTx`,
+  VOTE: `VoteTx`,
+  DEPOSIT: `DepositTx`,
+  CLAIM_REWARDS: `ClaimRewardsTx`,
+  SUBMIT_PROPOSAL: `SubmitProposalTx`,
+  UNKNOWN: `UnknownTx`,
+}
+
+module.exports = { lunieMessageTypes }
---
+++ vue/src/components/TableContainer.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <table v-if="length" class="table">
+      <thead>
+        <TableHeader :sort="sort" :properties="columns" :show-row-count="showRowCount" />
+      </thead>
+      <tbody>
+        <slot></slot>
+      </tbody>
+    </table>
+    <template v-else-if="!length">
+      <slot name="empty">
+        <tr class="no-results">
+          No Results
+        </tr>
+      </slot>
+    </template>
+  </div>
+</template>
+
+<script>
+import TableHeader from '@/components/TableHeader'
+export default {
+  name: 'TableContainer',
+  components: { TableHeader },
+  props: {
+    length: {
+      type: Number,
+      default: () => 0,
+    },
+    columns: {
+      type: Array,
+      required: true,
+    },
+    search: {
+      type: Boolean,
+      default: false,
+    },
+    sort: {
+      type: Object,
+      default: () => {},
+    },
+    loaded: {
+      type: Boolean,
+      default: false,
+    },
+    showRowCount: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      searchTerm: false,
+    }
+  },
+}
+</script>
+
+<style scoped>
+table {
+  table-layout: auto;
+  border-collapse: collapse;
+  min-width: 100%;
+}
+
+.no-results {
+  padding: 2rem;
+  height: 4rem;
+  display: table-cell;
+  /*color: var(--txt);*/
+  /*font-size: var(--text-sm);*/
+}
+
+.no-results h2 {
+  font-weight: 600;
+  padding-bottom: 0.5rem;
+  /*color: var(--bright);*/
+  /*font-size: var(--text-lg);*/
+}
+
+thead {
+  /*border-bottom: 1px solid var(--bc-dim);*/
+}
+</style>
