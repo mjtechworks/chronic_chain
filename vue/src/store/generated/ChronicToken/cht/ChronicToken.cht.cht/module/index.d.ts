c4787cb319ffa00a85f51b120f201eb5460362f5
Carlos <upworkbiesystems@gmail.com>
2022-01-26 17:38:51 -0500

    bug fixed

--- vue/src/store/generated/ChronicToken/cht/ChronicToken.cht.cht/module/index.d.ts
+++ vue/src/store/generated/ChronicToken/cht/ChronicToken.cht.cht/module/index.d.ts
@@ -1,13 +1,13 @@
 import { StdFee } from "@cosmjs/launchpad";
 import { Registry, OfflineSigner, EncodeObject } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
-import { MsgMigrateContract } from "./types/cht/tx";
 import { MsgStoreCode } from "./types/cht/tx";
-import { MsgExecuteContract } from "./types/cht/tx";
-import { MsgInstantiateContract } from "./types/cht/tx";
-import { MsgClearAdmin } from "./types/cht/tx";
-import { MsgUpdateAdmin } from "./types/cht/tx";
 import { MsgIBCCloseChannel } from "./types/cht/ibc";
+import { MsgClearAdmin } from "./types/cht/tx";
+import { MsgMigrateContract } from "./types/cht/tx";
+import { MsgUpdateAdmin } from "./types/cht/tx";
+import { MsgInstantiateContract } from "./types/cht/tx";
+import { MsgExecuteContract } from "./types/cht/tx";
 import { MsgIBCSend } from "./types/cht/ibc";
 export declare const MissingWalletError: Error;
 export declare const registry: Registry;
@@ -20,13 +20,13 @@ interface SignAndBroadcastOptions {
 }
 declare const txClient: (wallet: OfflineSigner, { addr: addr }?: TxClientOptions) => Promise<{
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }?: SignAndBroadcastOptions) => any;
-    msgMigrateContract: (data: MsgMigrateContract) => EncodeObject;
     msgStoreCode: (data: MsgStoreCode) => EncodeObject;
-    msgExecuteContract: (data: MsgExecuteContract) => EncodeObject;
-    msgInstantiateContract: (data: MsgInstantiateContract) => EncodeObject;
-    msgClearAdmin: (data: MsgClearAdmin) => EncodeObject;
-    msgUpdateAdmin: (data: MsgUpdateAdmin) => EncodeObject;
     msgIBCCloseChannel: (data: MsgIBCCloseChannel) => EncodeObject;
+    msgClearAdmin: (data: MsgClearAdmin) => EncodeObject;
+    msgMigrateContract: (data: MsgMigrateContract) => EncodeObject;
+    msgUpdateAdmin: (data: MsgUpdateAdmin) => EncodeObject;
+    msgInstantiateContract: (data: MsgInstantiateContract) => EncodeObject;
+    msgExecuteContract: (data: MsgExecuteContract) => EncodeObject;
     msgIBCSend: (data: MsgIBCSend) => EncodeObject;
 }>;
 interface QueryClientOptions {
